d8289a8265fbc1107e54b523051d6878802fbfc2
Marcel Joschko <marceljoschko@outlook.com>
2025-02-05 03:03:34 +0100

    changes

--- src/routes/post/[slug]/+page.server.ts
+++ src/routes/post/[slug]/+page.server.ts
@@ -19,21 +19,21 @@ export const load: PageServerLoad = async ({ params }) => {
 		const post = fm<Demo>(fs.readFileSync(`src/content/posts/${params.slug}.md`, 'utf-8'));
 
 		console.log(`processing markdown src/content/${params.slug}.md`);
-
+		console.log(post);
 		if (post) {
 			const hast = toHast(fromMarkdown(post.body));
-
+			console.log(hast);
 			return {
 				title: post.attributes.title,
 				description: post.attributes.description,
 				image: await getImageProperties(post.attributes.image),
-				optional: post.attributes.optional,
+				optional: post.attributes?.optional,
 				body: hast && (await addImagePropertiesToHast(reduceHast(hast)))
 			};
 		}
 		error(500, 'something wrong with the markdown file');
-	} catch (err){
-		console.error(err)
+	} catch (err) {
+		console.error(err);
 		error(404, 'not found');
 	}
 };
